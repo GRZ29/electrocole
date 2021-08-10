7101d71debc1607ccb5f681e7a08ff6d8ee03d06
GRZ <rosalesgerald198@gmail.com>
2021-08-10 14:49:58 -0600

    se modifico un espacio

--- src/componentes/ArchivoDescargado.js
+++ src/componentes/ArchivoDescargado.js
@@ -7,7 +7,7 @@ const ArchivoDescargado = () => {
     return (
         <div  id="divdescarga" style={{height:350,textAlign:"center"}}>
             <br/><br/>
-                <h2>QUIERES INFORMARTE MAS? TE DEJAMOS NUESTRO PROGRAMA DE ESTUDIO  </h2>
+                <h2>QUIERES INFORMARTE MAS? TE DEJAMOS NUESTRO PROGRAMA DE ESTUDIO</h2>
             <br/><br/><br/><br/><br/>
                 <a style={{fontSize:40}} id="link_descargas"  href={pdf} download="Programa_Electronica_Industrial.pdf">Descargar</a>
         </div>
