40992ef9110afb12e924735cfc74f82837a8037f
electrovoca <88790586+electrovoca@users.noreply.github.com>
2021-08-11 11:36:26 -0600

    Update Aprendizaje.js

--- src/componentes/Aprendizaje.js
+++ src/componentes/Aprendizaje.js
@@ -9,7 +9,7 @@ const Acerca = () => {
             <section className="page-section" id="about">
             <div className="container">
                 <div className="text-center">
-                    <h2 className="section-heading text-uppercase">Nivel de aprendizaje</h2>
+                    <h2 className="section-heading text-uppercase">Nivel de aprendizaje </h2>
                     <br/><br/>
                 </div>
                 <ul className="timeline">
